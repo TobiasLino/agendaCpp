9bd62e88093ec621bb22eeec8026932d64b05c52
TobiasLino <tobias.lino@fatec.sp.gov.br>
2020-02-27 19:59:30 -0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/agendaCpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/agendaCpp.dir/main.cpp.o"
+  "agendaCpp"
+  "agendaCpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/agendaCpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/agendaCpp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for agendaCpp.
+# This may be replaced when dependencies are built.
