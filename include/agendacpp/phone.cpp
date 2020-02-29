1dd9b19017b4b3d50530765b09140b9ed640f835
TobiasLino <tobias.lino@fatec.sp.gov.br>
2020-02-29 19:11:14 -0300

    Tratando erros de inserção do número.

--- include/agendacpp/phone.cpp
+++ include/agendacpp/phone.cpp
@@ -1,15 +1,23 @@
 // Copyright 2020 Tobias
 
 #include "phone.hpp"
+#include "errors.hpp"
+
+Errors err;
 
 Phone::Phone() {
   strcpy(number_, "00000000");
 }
 Phone::Phone(std::string number) {
-  strcpy(this->number_, number.c_str());
+  int i = snprintf(this->number_, NUMBERLEN+1, "%s", number.c_str());
+  if (i < 0 || i > NUMBERLEN+1) {
+    err.Error("Writing to number");
+  }
 }
 void Phone::set_phone(std::string number) {
-  strcpy(this->number_, number.c_str());
+  int i = snprintf(this->number_, NUMBERLEN+1, "%s", number.c_str());
+  if (i < 0 || i > NUMBERLEN+1) {
+    err.Error("Writing to number");
 }
 char* Phone::get_phone() {
   return number_;
